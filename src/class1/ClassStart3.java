ef5a63628a8c660d90801464ff2a92a816fe2c10
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-02-05 12:11:41 +0900

    클래스 도입

--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -8,13 +8,16 @@ public class ClassStart3 {
 		student1.age = 15;
 		student1.grade = 80;
 
-		Student student2 = new Student();
+		Student student2 = new Student(); //Student 객체 생성 + 참조값 저장
 		student2.name = "학생2";
 		student2.age = 16;
 		student2.grade = 70;
 
 		System.out.println("이름:" + student1.name + " 나이:" + student1.age + " 성적:" + student1.grade);
 		System.out.println("이름:" + student2.name + " 나이:" + student2.age + " 성적:" + student2.grade);
+
+		System.out.println(student1); //class1.Student@19469ea2 @뒤의 값이 참조값으로 보면된다.
+		System.out.println(student2);
 	}
 }
 

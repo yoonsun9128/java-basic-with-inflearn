c53ce342165b9d15f3aff2223353a69e8bf6af3e
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-02-10 15:28:13 +0900

    변수와 초기화

---
+++ src/ref/InitMain.java
@@ -0,0 +1,9 @@
+package ref;
+
+public class InitMain {
+	public static void main(String[] args) {
+		InitData data = new InitData();
+		System.out.println(data.value);
+		System.out.println(data.value2);
+	}
+}

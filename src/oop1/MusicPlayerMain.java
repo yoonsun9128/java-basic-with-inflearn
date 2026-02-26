57cc58a3a401ca6f9e50e0672f68f8b19b6ee32f
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-02-26 16:37:08 +0900

    객체 지향 프로그래밍

---
+++ src/oop1/MusicPlayerMain.java
@@ -0,0 +1,18 @@
+package oop1;
+
+public class MusicPlayerMain {
+	public static void main(String[] args) {
+		MusicPlayer player = new MusicPlayer();
+
+		player.on();
+
+		player.volumeUp();
+		player.volumeUp();
+
+		player.volumeDown();
+
+		player.showStatus();
+
+		player.off();
+	}
+}

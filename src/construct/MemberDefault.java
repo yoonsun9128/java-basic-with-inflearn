5c44adb751871f211b11f472ea4edff481e4c015
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-03-03 15:09:57 +0900

    기본 생성자

---
+++ src/construct/MemberDefault.java
@@ -0,0 +1,7 @@
+package construct;
+
+public class MemberDefault {
+	String name;
+
+	//원칙적으로는 생성자는 만들던 만들지 않던 무조건 호출이 되고 있다.
+}
---
+++ src/construct/MemberDefaultMain.java
@@ -0,0 +1,7 @@
+package construct;
+
+public class MemberDefaultMain {
+	public static void main(String[] args) {
+		MemberDefault member1 = new MemberDefault();
+	}
+}

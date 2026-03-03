042d9eee5aaa434334e1ff0d88753850f19ea7ed
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-03-03 15:20:03 +0900

    생성자-오버로딩과 this

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,12 @@ public class MemberConstruct {
 	int age;
 	int grade;
 
+	//추가 - 생성자 오버로딩
+	MemberConstruct(String name, int age) {
+		//성적을 안 넣으면 50점 부터 시작한다.
+		this(name, age, 50);
+	}
+
 	//생성자 만듬
 	// 메서드와 비슷하지만 몇가지의 차이가 있음
 	// -> 생성자의 이름은 클래스 이름과 같아야한다. 반환 타입이 없음

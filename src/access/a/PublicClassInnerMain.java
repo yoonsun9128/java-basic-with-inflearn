df840829eda6b7f3f20d02062745a810bccff0b2
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-03-04 20:25:11 +0900

    접근 제어자 사용 - 클래스 레벨

---
+++ src/access/a/PublicClassInnerMain.java
@@ -0,0 +1,11 @@
+package access.a;
+
+public class PublicClassInnerMain {
+	public static void main(String[] args) {
+		PublicClass publicClass = new PublicClass();
+		DefaultClass1 class1 = new DefaultClass1();
+		DefaultClass2 class2 = new DefaultClass2();
+	}
+}
+
+//패키치 위치가 같아서 접근 가능

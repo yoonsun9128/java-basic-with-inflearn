2c7dd44b7c582099c5acf41558ffee975d43ae1d
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-04-30 15:05:26 +0900

    캐스팅의 종류

---
+++ src/poly/basic/CastingMain2.java
@@ -0,0 +1,18 @@
+package poly.basic;
+
+public class CastingMain2 {
+	public static void main(String[] args) {
+		//parent -> chlid
+		Parent poly = new Child();
+//		poly.childMethod();
+
+		//다운캐스팅 (부모 타입 -> 자식 타입)
+		// 기존의 poly를 그대로 사용하는게 아니라 복사해서 사용하고 있다!
+		Child chlid = (Child) poly; //자식은 속이 쫍아서 담을 수 없다.but 자바에서는 경고를 하고 있으나 개발자가 지시 할 수 있다
+		chlid.childMethod();
+
+		//일시적 다운캐스팅 - 해당 메서드를 호출하는  순간만 다운 캐스팅
+		((Child) poly).childMethod();
+	}
+}
+
---
+++ src/poly/basic/CastingMain3.java
@@ -0,0 +1,12 @@
+package poly.basic;
+
+public class CastingMain3 {
+	public static void main(String[] args) {
+		//upcasting vs downcasting
+		Child child = new Child();
+		Parent parent1 = child; //upcasting은 생략을 권장 한다.
+
+
+	}
+}
+
